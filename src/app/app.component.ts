9a38d09aa60e93fd23ac596739b7f9989bd46634
ShpikOne <Shibut98@gmail.com>
2020-11-02 20:23:33 +0300

    added note editing and tag parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
+import { Note } from './note';
 import { Component } from '@angular/core';
 
+const tagRegexp = new RegExp("(^|\\s)(#.+?)\\b","g");
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-notes';
+
+  mock: Note[] = [{
+    text:"bbb",
+    tags:[]
+  },{
+      text:"vvv",
+      tags:[]
+  }];
+
+  notes:Note[] = this.mock;
+  currNote:Note;
+  oldText = "";
+  filterTags:string[] = [];
+
+  ngDoCheck() {
+    if(this.currNote){
+      if(this.currNote.text != this.oldText){
+        this.noteParseAndSetTags(this.currNote);
+        this.oldText = this.currNote.text;
+      }
+    }
+  }
+
+  noteParseAndSetTags(note:Note){
+    const tags = this.noteParseTags(note);
+    note.tags = tags;
+  }
+
+  noteParseTags(note:Note):string[]{
+    const matches = note.text.match(tagRegexp);
+    if (matches){
+      matches.forEach((tag)=>tag.trim());
+      return matches;
+    }
+    else
+      return [];
+  }
+
+
+  onSelect(note:Note){
+    this.currNote = note;
+  }
 }
---
+++ src/app/note.ts
@@ -0,0 +1,4 @@
+export class Note{
+    text: string;
+    tags: string[];
+}
